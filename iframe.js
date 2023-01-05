4863ebead774eccfd9a13eab38cdc6702d405f1a
ranjitn85 <ranjitn.85@gmail.com>
2023-01-05 12:16:33 +0800

    Add files via upload

---
+++ iframe.js
@@ -0,0 +1,129 @@
+import { _ as _decorate, s, i, e, y, a as e$1 } from './query-assigned-elements-14717414.js';
+
+let ZwcIframe = _decorate([e$1('zwc-iframe')], function (_initialize, _LitElement) {
+  class ZwcIframe extends _LitElement {
+    constructor(...args) {
+      super(...args);
+
+      _initialize(this);
+    }
+
+  }
+
+  return {
+    F: ZwcIframe,
+    d: [{
+      kind: "field",
+      static: true,
+      key: "styles",
+
+      value() {
+        return i`
+    :host {
+      height: 100%;
+      width: 100%;
+      display: block;
+    }
+
+    .frame {
+      display: inline-block;
+      height: 100%;
+      width: 100%;
+      background-color: transparent;
+      border: none;
+    }
+  `;
+      }
+
+    }, {
+      kind: "field",
+      decorators: [e()],
+      key: "name",
+
+      value() {
+        return 'Hello';
+      }
+
+    }, {
+      kind: "field",
+      decorators: [e()],
+      key: "title",
+
+      value() {
+        return 'Hello';
+      }
+
+    }, {
+      kind: "field",
+      decorators: [e()],
+      key: "src",
+
+      value() {
+        return 'https://stackoverflow.com/';
+      }
+
+    }, {
+      kind: "field",
+      decorators: [e({
+        type: Number
+      })],
+      key: "height",
+
+      value() {
+        return 500;
+      }
+
+    }, {
+      kind: "method",
+      static: true,
+      key: "getMetaConfig",
+      value: // Define scoped styles right with your component, in plain CSS
+      function getMetaConfig() {
+        // plugin contract information
+        return {
+          controlName: 'IFrame-new',
+          fallbackDisableSubmit: false,
+          description: 'IFrame component which can render url view with the frame',
+          iconUrl: 'one-line-text',
+          groupName: 'Visual',
+          version: '1.3',
+          properties: {
+            src: {
+              type: 'string',
+              title: 'Source URL',
+              description: 'URL of the iframe, please note many sites block been rendered in iframes'
+            },
+            height: {
+              type: 'string',
+              title: 'Height',
+              description: 'Height of the component'
+            }
+          },
+          standardProperties: {
+            readOnly: true,
+            required: true,
+            description: true
+          }
+        };
+      } // Render the UI as a function of component state
+
+    }, {
+      kind: "method",
+      key: "render",
+      value: function render() {
+        console.log('Props', {
+          name: this.name,
+          title: this.title,
+          src: this.src,
+          height: this.height
+        });
+        ({
+          height: this.height + 'px'
+        });
+        return y`<p>Hello World !</p>${this.name}`;
+      }
+    }]
+  };
+}, s);
+
+export { ZwcIframe };
